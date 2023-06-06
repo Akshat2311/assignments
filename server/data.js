d70df0dbb2c5006efe17dc4204bd2f5953f6fc18
Akshat2311 <AKSHAT.MISHRA319@GMAIL.COM>
2023-06-06 13:17:57 +0530

    react configuration added

--- server/data.js
+++ server/data.js
@@ -1,5 +1,14 @@
+import bcrypt from "bcryptjs"
 export const users = [
     {
-        
+        name:"Aditya",
+        phone_number:"8770471714",
+        password:bcrypt.hashSync('123456', 10)
+    },
+    {
+        name: "Akshat",
+        phone_number: "7389074391",
+        password: bcrypt.hashSync('123456', 10)
     }
 ]
+
